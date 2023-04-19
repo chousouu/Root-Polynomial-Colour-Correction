7aa251d9cb039c2304de5063f21a2a19e2755585
Yasin Mamedov <mamedov.iakh@phystech.edu>
2023-04-20 02:44:13 +0300

    rewrite getRPVECTOR

--- RPCC.py
+++ RPCC.py
@@ -30,21 +30,23 @@ def filtered_P_combs(sum, length = 3, combs = np.array([[0, 0, 0]]), comb = np.a
 # TODO: rewrite?
 #extended by adding additional rpcc terms
 def getRPVector(responses, degree, bias = False):
-    #responses  : N responses (meaning Nx3 RGB responses)
-    # returns NxM vector, where M - amount of terms of polynom of degree == degree 
-    R = responses[:, 0]
-    G = responses[:, 1]
-    B = responses[:, 2]
+    #responses  : 3xN responses (meaning 3xN RGB responses)
+    # returns MxN vector, where M - amount of terms of polynom of degree == degree 
 
-    print(f"B = {B.shape}, R = {R.shape}")
+    R = responses[0, :]
+    G = responses[1, :]
+    B = responses[2, :]
+
 #!!!!TODO: change to M = **__func that counts amount like the one with PCC__**
     M = (RP_combs(degree).shape)[0]
+
     expanded = np.zeros((1, M)) # needs to be NxM
     combs = RP_combs(degree)
     rows = R.shape[0]
-    expanded = np.zeros(shape=(1, R.shape[0]))
+    expanded = np.zeros(shape=(1, rows))
     for term in combs: # term e.g rg  == [1, 1, 0]
-        elem = np.ones(shape = (1, R.shape[0]))
+        print(f"term = {term.shape}")
+        elem = np.ones(shape = (1, rows))
 #TODO: govnocode
         if(term[0] != 0):
             elem *= R**term[0]
@@ -58,7 +60,8 @@ def getRPVector(responses, degree, bias = False):
     # if bias == True : 
     #     expanded = np.append(expanded, np.ones(expanded.shape[1]))
 
-    return expanded[1:].T
+    print("expanded", expanded.shape)
+    return expanded[1:]
 
 def RP_combs(degree):
     RP_v = filtered_P_combs(degree)
@@ -74,49 +77,59 @@ def CalcTFactor():
     pass
     # return t_factor
 
-def GetCCM(Q, R, t_factor = 0):
-    # Q : reflectances Nx3
-    # R : camera resp  Nx3
+def GetCCM(Q, R, degree, t_factor = 0):
+    # Q : image/colorchecker reflectances 3xN
+    # R : image/colorchecker responses 3xN 
+    # return : ccm : 3xM 
+
+    print(f"Q befpre = {Q.shape}")
+    Q = PreprocessImg(Q)
+    print(f"Q afyer = {Q.shape}")
+
+    print(f"R befpre = {R.shape}")
+    R = PreprocessImg(R)
+    print(f"R AFTER = {R.shape}")
+
 
     # if (t_factor != 0):
     #     t_factor = CalcTFactor()
-
-    RxRT = np.dot(R.T, R)  # 3XN * NX3
-    R_TxQ = np.dot(R.T, Q) # 3XN * NX3
+    R = getRPVector(R, degree) # M x N (N responses, M terms)
+    print("r", R.shape)
+    RxRT = np.dot(R, R.T) #  MxN * NxM = MxM
+    QxRT = np.dot(Q, R.T) #  3XN * NxM = 3xM
 
     print(f"det = {np.linalg.det(RxRT + t_factor*np.eye(RxRT.shape[0]))}")
 
-    ccm = np.dot(np.linalg.inv(RxRT + t_factor*np.eye(RxRT.shape[0])), R_TxQ)  
-    #3XN * NX3 * 3XN*NX3 = 3X3
+    print(f"QxRT = {QxRT.shape}, RxRT = {RxRT.shape}")
+
+    ccm = np.dot(QxRT, np.linalg.inv(RxRT + t_factor*np.eye(RxRT.shape[0])))  
     return ccm
 
-def ApplyCCM(image, camera_responses, reflectances, degree, scale = 1, t_factor = 2.3):
+def ApplyCCM(ccm, camera_responses, degree : int, scale = 1):
     # camera_responses : Nx3 array containing r,g,b responses (in range 0-1) of img 
     # ^ need to change cuz picture is NxMx3
     # reflectances : Nx3 array containing x,y,z (in range 0-1)
     # scale : camera responses * scale first, then __expanded__ and multiplied 
 
-    camera_responses *= scale
-    camera_responses = np.clip(camera_responses, 0, 1)
+    #NEW 
+    # ccm - matrix 3xM, M = terms counter of chosen degree 
 
-    #preprocess data
+    camera_responses = np.clip(camera_responses * scale, 0, 1)
 
-    rgb_x = getRPVector(image, degree) #image expanded
+    rho = getRPVector(camera_responses, degree) #image expanded
 
-    R = getRPVector(camera_responses, degree) # camera responses expanded to root polynom of degree 
-                                              # needed for ccm
-    ccm = GetCCM(Q = reflectances, R = R, t_factor = t_factor)
+    predicted_xyz = np.dot(ccm, rho)
 
-    print(rgb_x.shape, ccm.shape)
-    predicted_responses = rgb_x.dot(ccm)
-
-    return predicted_responses #predicted?
+    return predicted_xyz #predicted?
 
 def PreprocessImg(image):
     if (np.ndim(image) == 3):
         image = np.reshape(image, (image.shape[0] * image.shape[1], 3))
-    if (len(image[image > 1])):
-        image = image / 255 #normalize input
+    elif ((np.ndim(image) == 2)):
+        if((image.shape[0] != 3) and (image.shape[1] == 3)):
+            image = image.T
+    # if (len(image[image > 1])):
+    #     image = image / 255 #normalize input
     return image
 
 # HSI_PATH = "/home/yasin/iitp/interview/li_ds/"
@@ -124,6 +137,7 @@ def PreprocessImg(image):
 
 
 def main():
+    DEGREE = 4
 
     HSI_data = GetData.CheckerSpectrum()[0]
     (ydim, xdim, zdim) = HSI_data.shape
@@ -142,7 +156,27 @@ def main():
 
     XYZ_image = HSI.HSI2XYZ(wl, HSI_data, xdim, ydim)
 
+    print(XYZ_image.shape)
+
+    XYZ_image = np.reshape(XYZ_image, [XYZ_image.shape[0] * XYZ_image.shape[1],XYZ_image.shape[2]])
+
+    print(XYZ_image.shape)
+
     RGB_IMAGE = HSI.HSI2RGB(wl, HSI_data, xdim, ydim)
+
+    print(RGB_IMAGE.shape)
+
+    RGB_IMAGE = np.reshape(RGB_IMAGE, [RGB_IMAGE.shape[0] * RGB_IMAGE.shape[1],RGB_IMAGE.shape[2]])
+
+    print(RGB_IMAGE.shape)
+
+    ccm = GetCCM(XYZ_image, RGB_IMAGE, DEGREE)
+
+    print(f"ccm = {ccm.shape}")
+
+    xyz_updated = ApplyCCM(ccm, RGB_IMAGE, DEGREE)
+
+
 
 
     plt.subplots(1 , 2, figsize=(20, 20))
@@ -154,7 +188,7 @@ def main():
     plt.yticks([])
     plt.subplot(1, 2, 2)
     plt.title("XYZ")
-    plt.imshow(XYZ_image)
+    plt.imshow(xyz_updated)
     plt.grid(False)
     plt.xticks([])
     plt.yticks([])
