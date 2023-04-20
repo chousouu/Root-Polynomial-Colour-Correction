0682abf8e74d413f673d6edd04abbfb8de34a9f4
Yasin Mamedov <mamedov.iakh@phystech.edu>
2023-04-20 13:05:08 +0300

    now works on colorchecker data, doing for hyperspectral image in general

--- RPCC.py
+++ RPCC.py
@@ -30,31 +30,38 @@ def filtered_P_combs(sum, length = 3, combs = np.array([[0, 0, 0]]), comb = np.a
 # TODO: rewrite?
 #extended by adding additional rpcc terms
 def getRPVector(responses, degree, bias = False):
-    #responses  : 3xN responses (meaning 3xN RGB responses)
+    #responses : 3xN responses (meaning 3xN RGB responses)
     # returns MxN vector, where M - amount of terms of polynom of degree == degree 
+    responses = PreprocessImg(responses)
 
     R = responses[0, :]
     G = responses[1, :]
     B = responses[2, :]
 
-#!!!!TODO: change to M = **__func that counts amount like the one with PCC__**
-    M = (RP_combs(degree).shape)[0]
+    combs = RP_combs(degree) # 22x3, M = 22 , N = 3 ([r, g, b])
 
-    expanded = np.zeros((1, M)) # needs to be NxM
-    combs = RP_combs(degree)
-    rows = R.shape[0]
-    expanded = np.zeros(shape=(1, rows))
-    for term in combs: # term e.g rg  == [1, 1, 0]
-        print(f"term = {term.shape}")
-        elem = np.ones(shape = (1, rows))
-#TODO: govnocode
-        if(term[0] != 0):
-            elem *= R**term[0]
-        if(term[1] != 0):
-            elem *= G**term[1]
-        if(term[2] != 0):
-            elem *= B**term[2]
-        expanded = np.append(expanded, elem, axis = 0)
+#!!!!TODO: change to M = **__func that counts amount like the one with PCC__**
+    
+    M = combs.shape[0] # amount of terms
+    N = R.shape[0] #amount of responses
+    
+    print("shapes", M, N)
+
+    expanded = np.zeros(shape=(1, N)) # needs to be NxM
+
+    for i in range(M):
+        elem = np.ones(shape=(1, N))
+        if(combs[i][0] !=  0):
+            elem *= R ** combs[i][0]
+        if(combs[i][1] !=  0):
+            elem *= G ** combs[i][1]
+        if(combs[i][2] !=  0):
+            elem *= B ** combs[i][2]
+        expanded = np.append(expanded, elem, axis=0)
+    
+    return expanded[1:, ]
+
+#////////////// TODO:REWRITE
 
     # Not Sure
     # if bias == True : 
@@ -64,6 +71,7 @@ def getRPVector(responses, degree, bias = False):
     return expanded[1:]
 
 def RP_combs(degree):
+    """returns array (3 , terms of Degree)"""
     RP_v = filtered_P_combs(degree)
 
     expanded = np.zeros((1, 3)) # 3 cuz represents R^x, G^y, B^z
@@ -78,21 +86,18 @@ def CalcTFactor():
     # return t_factor
 
 def GetCCM(Q, R, degree, t_factor = 0):
-    # Q : image/colorchecker reflectances 3xN
-    # R : image/colorchecker responses 3xN 
+    # Q : image/colorchecker reflectances
+    # R : image/colorchecker responses 
     # return : ccm : 3xM 
 
     print(f"Q befpre = {Q.shape}")
-    Q = PreprocessImg(Q)
+    Q = PreprocessImg(Q) #3xN
     print(f"Q afyer = {Q.shape}")
 
     print(f"R befpre = {R.shape}")
-    R = PreprocessImg(R)
-    print(f"R AFTER = {R.shape}")
+    R = PreprocessImg(R) # 3xN
+    print(f"R AFTER = {R.shape}") 
 
-
-    # if (t_factor != 0):
-    #     t_factor = CalcTFactor()
     R = getRPVector(R, degree) # M x N (N responses, M terms)
     print("r", R.shape)
     RxRT = np.dot(R, R.T) #  MxN * NxM = MxM
@@ -114,22 +119,28 @@ def ApplyCCM(ccm, camera_responses, degree : int, scale = 1):
     #NEW 
     # ccm - matrix 3xM, M = terms counter of chosen degree 
 
+    camera_responses = PreprocessImg(camera_responses)  
+
     camera_responses = np.clip(camera_responses * scale, 0, 1)
 
+    print("responses before", camera_responses.shape)
     rho = getRPVector(camera_responses, degree) #image expanded
+    print("responses after", camera_responses.shape)
+
+    print(f"rho {rho.shape}")
 
     predicted_xyz = np.dot(ccm, rho)
+
+    print(f"{predicted_xyz.shape} = {ccm.shape} @ {rho.shape}")
 
     return predicted_xyz #predicted?
 
 def PreprocessImg(image):
     if (np.ndim(image) == 3):
-        image = np.reshape(image, (image.shape[0] * image.shape[1], 3))
+        image = np.reshape(image, (image.shape[2], image.shape[0] * image.shape[1]))
     elif ((np.ndim(image) == 2)):
         if((image.shape[0] != 3) and (image.shape[1] == 3)):
             image = image.T
-    # if (len(image[image > 1])):
-    #     image = image / 255 #normalize input
     return image
 
 # HSI_PATH = "/home/yasin/iitp/interview/li_ds/"
@@ -144,7 +155,7 @@ def main():
     # Load wavelengths of hyperspectral data
     wl = GetData.CheckerSpectrum()[1]
 
-        #TODO: fix so works on random pics
+    #     #TODO: fix so works on random pics
     # h5file = h5py.File("/home/yasin/iitp/interview/li_ds/2019-09-18_003.h5", 'r')
     # HSI_data = h5file['img\\']
     # HSI_data = np.reshape(HSI_data, [HSI_data.shape[1], HSI_data.shape[2], HSI_data.shape[0]])
@@ -156,39 +167,40 @@ def main():
 
     XYZ_image = HSI.HSI2XYZ(wl, HSI_data, xdim, ydim)
 
-    print(XYZ_image.shape)
+    # XYZ_image = np.reshape(XYZ_image, [XYZ_image.shape[0] * XYZ_image.shape[1],XYZ_image.shape[2]])
 
-    XYZ_image = np.reshape(XYZ_image, [XYZ_image.shape[0] * XYZ_image.shape[1],XYZ_image.shape[2]])
-
-    print(XYZ_image.shape)
+    print("xyzim", XYZ_image.shape)
 
     RGB_IMAGE = HSI.HSI2RGB(wl, HSI_data, xdim, ydim)
 
-    print(RGB_IMAGE.shape)
+    print("rgbim", RGB_IMAGE.shape)
 
-    RGB_IMAGE = np.reshape(RGB_IMAGE, [RGB_IMAGE.shape[0] * RGB_IMAGE.shape[1],RGB_IMAGE.shape[2]])
+    # RGB_IMAGE = np.reshape(RGB_IMAGE, [RGB_IMAGE.shape[0] * RGB_IMAGE.shape[1],RGB_IMAGE.shape[2]])
 
-    print(RGB_IMAGE.shape)
+    # print("reshaped image, ", RGB_IMAGE.shape)
 
-    ccm = GetCCM(XYZ_image, RGB_IMAGE, DEGREE)
+    ccm = GetCCM(XYZ_image, RGB_IMAGE, DEGREE, t_factor = 0)
 
     print(f"ccm = {ccm.shape}")
 
-    xyz_updated = ApplyCCM(ccm, RGB_IMAGE, DEGREE)
 
+    print('APPLYING') 
+    xyz_pred = ApplyCCM(ccm, RGB_IMAGE, DEGREE)
 
-
+    #show results
+    xyz_pred = np.reshape(xyz_pred,(4, 6, 3))
+    RGB_IMAGE = np.reshape(RGB_IMAGE, (4, 6, 3))
 
     plt.subplots(1 , 2, figsize=(20, 20))
     plt.subplot(1, 2, 1)
-    plt.title("RGB")
-    plt.imshow(RGB_IMAGE)
+    plt.title("XYZ_REAL")
+    plt.imshow(XYZ_image)
     plt.grid(False)
     plt.xticks([])
     plt.yticks([])
     plt.subplot(1, 2, 2)
-    plt.title("XYZ")
-    plt.imshow(xyz_updated)
+    plt.title("XYZ_PRED")
+    plt.imshow(xyz_pred)
     plt.grid(False)
     plt.xticks([])
     plt.yticks([])
